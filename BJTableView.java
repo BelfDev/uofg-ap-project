dbf5ad1c66f82a9082f3d4c14983fb48d3db0480
Pedro Belfort <belfortdev@gmail.com>
2020-04-01 17:55:12 -0300

    Adds BJTableView docs

--- BJTableView.java
+++ BJTableView.java
@@ -3,6 +3,10 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class encapsulates all the content displayed on the
+ * black jack game table. Namely: all the player views.
+ */
 class BJTableView extends JPanel {
 
     private List<PlayerView> playerViews;
@@ -10,6 +14,9 @@ class BJTableView extends JPanel {
 
     private DealerView dealerView;
 
+    /**
+     * Constructs the black jack table content.
+     */
     public BJTableView() {
         this.playerViews = new ArrayList<>();
         this.backgroundImage = Utils.loadImage("assets/bj-table.png");
@@ -17,19 +24,43 @@ class BJTableView extends JPanel {
         addDealer();
     }
 
+    // GETTERS
+
     public DealerView getDealerView() {
         return dealerView;
     }
 
+    // BACKGROUND APPEARANCE
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Draws the background image.
+        g.drawImage(backgroundImage, -2, -2, getWidth() + 4, getHeight() + 4, this);
+    }
+
+    // PLAYERS
+
+    /**
+     * Adds a new PlayerView to the black jack table.
+     *
+     * @param name the text to be displayed underneath the player cards.
+     * @param slot the slot number.
+     * @return the newly-added PlayerView.
+     */
     public PlayerView addPlayer(String name, int slot) {
+        // Checks if all available slots have been claimed
         if (slot <= Configs.MAX_NUMBER_OF_PLAYERS) {
+            // Creates a new player view
             PlayerView playerView = new PlayerView(slot);
             if (name != null) {
                 playerView.setName(name);
             }
             playerViews.add(playerView);
+            // Sets the player location
             Point playerLocation = Configs.PLAYER_SLOT_LOCATIONS.get(slot);
             playerView.setLocation(playerLocation);
+            // Adds the player view
             add(playerView);
             refreshInterface();
             return playerView;
@@ -37,11 +68,18 @@ class BJTableView extends JPanel {
         return null;
     }
 
+    /**
+     * Removes a PlayerView from the game table.
+     *
+     * @param slot the slot that should be emptied.
+     */
     public void removePlayer(int slot) {
+        // Fetches the PlayerView placed on the given slot
         PlayerView playerView = playerViews.stream()
                 .filter(p -> p.getSlot() == (slot))
                 .findFirst()
                 .orElse(null);
+        // Removes the PlayerView
         if (playerView != null) {
             remove(playerView);
             playerViews.remove(playerView);
@@ -49,20 +87,17 @@ class BJTableView extends JPanel {
         refreshInterface();
     }
 
+    // CONVENIENCE METHODS
+
     private void addDealer() {
+        // Adds a new dealer view
         dealerView = new DealerView();
         dealerView.setLocation(Configs.DEALER_SLOT_LOCATION);
         add(dealerView);
     }
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        // Draw the background image.
-        g.drawImage(backgroundImage, -2, -2, getWidth() + 4, getHeight() + 4, this);
-    }
-
     private void refreshInterface() {
+        // Notifies Swing that the interface should be redrawn immediately
         revalidate();
         repaint();
     }
