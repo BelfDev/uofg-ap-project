dfa1be4e26349600bf3821f0cdbec6a9da76405b
Pedro Belfort <belfortdev@gmail.com>
2020-04-01 16:53:09 -0300

    Add RequestSender interface doc

--- RequestSender.java
+++ RequestSender.java
@@ -1,3 +1,7 @@
+/**
+ * This interface ensures that classes that intend to send out
+ * requests offer an implementation of the sendRequest method.
+ */
 public interface RequestSender {
     void sendRequest(ClientRequest request);
 }
