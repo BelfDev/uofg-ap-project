b751a3eded1003ed495dac3ea642f5da8d69c45b
Pedro Belfort <belfortdev@gmail.com>
2020-04-01 18:48:00 -0300

    Adds CardReceiver docs

--- CardReceiver.java
+++ CardReceiver.java
@@ -1,6 +1,14 @@
+/**
+ * This interface dictates which methods views should implement
+ * in order to be considered a card receiver and take advantage
+ * of abstracted methods throughout the code base.
+ */
 public interface CardReceiver {
     int getNumberOfCards();
+
     void addCard(CardView card);
+
     void removeCards();
+
     void setScore(int value);
 }
