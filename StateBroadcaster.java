671112d24dcba5a778a23857a09fb6253c59f6a0
Pedro Belfort <belfortdev@gmail.com>
2020-04-01 16:56:29 -0300

    Add StateBroadcaster doc

--- StateBroadcaster.java
+++ StateBroadcaster.java
@@ -1,15 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class encapsulates the logic to notify game state changes.
+ */
 public class StateBroadcaster {
     private List<StateListener> listeners = new ArrayList<>();
 
+    /**
+     * Adds a state listener to the listeners list.
+     *
+     * @param listener the state listener.
+     */
     public void addListener(StateListener listener) {
         listeners.add(listener);
     }
 
+    /**
+     * Notifies the given state to all observers.
+     *
+     * @param state new state to be broadcasted to observers.
+     */
     public void notifyNewState(GameState state) {
-        System.out.println("new state received");
         listeners.forEach(l -> l.onReceiveState(state));
     }
 
